92cadf1d634365b4e587e5c953c19dd274c2e21f
mulhamadil <molhamadel10@outlook.com>
2021-08-31 18:58:01 +0000

    test

--- index.test.js
+++ index.test.js
@@ -1,18 +1,5 @@
 const app = require("./index.js");
-const mongoose = require("mongoose");
 const supertest = require("supertest");
-
-beforeEach((done) => {
-  mongoose.connect("mongodb://localhost:27017/JestDB",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => done());
-});
-
-afterEach((done) => {
-  mongoose.connection.db.dropDatabase(() => {
-    mongoose.connection.close(() => done())
-  });
-});
 
 test("GET /collect/0800097-04.2017.8.10.0135", async () => {
   
